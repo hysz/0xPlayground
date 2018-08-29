7c349e81f99c08ba9394182e728b840709075021
Greg Hysen <greg.hysen@gmail.com>
2018-08-29 16:07:06 -0700

    Better check with some initial thought comments

--- match_order_tests.py
+++ match_order_tests.py
@@ -27,6 +27,12 @@ def isRoundingErrorCeil(numerator, denominator, target):
 def needsRounding(numerator, denominator, target):
   return (numerator * target) % denominator != 0
 
+def getPartialAmountFloor(numerator, denominator, target):
+  return math.floor((numerator * target) / denominator)
+
+def getPartialAmountCeil(numerator, denominator, target):
+  return math.ceil((numerator * target) / denominator)
+
 class Candidate:
   def __init__(self):
     self.leftMaker = 0
@@ -90,16 +96,85 @@ def hasRoundingErrorFloor_RightFillsLeft(c):
 def noRoundingErrorFloor_RightFillsLeft(c):
   return isRoundingErrorFloor(*rightBuyParameters_RightFillsLeft(c)) == False
 
-# Returns True iff there exists a `leftMaker` that could be returned by `Exchange.fillOrder`,
-# given `rightMaker`, `rightTaker`, and `leftTaker
-def isValidFillResult_Right(c):
-  for i in range(1, c.rightTaker + 1):
-    if math.floor((c.rightMaker / c.rightTaker) * i) == c.leftTaker:
+
+# There are cases where order matching creates fill amounts [amountToSell, amountToBuy]
+# that are unattainable from Exchange.fillOrder.
+# This function returns true iff matching the orders in `c` would yield fill amounts for the right order 
+# that are unattainable by Exchange.fillOrder.
+def matchingResultIsAttainableByFillOrder_RightFillsLeft(c):
+  # The left order is fully filled by the right order,
+  # so the right maker must sell c.leftTaker units of their asset.
+  #
+  # In return the right maker receives math.ceil((c.rightTaker/c.rightMaker) * c.leftTaker) units
+  # of the left maker's asset.
+  #
+  # The question: is it possible for these fill amounts to be obtained by calling Exchange.fillOrder?
+  # 
+  # Valid fill results produced by fillOrder follow a stepwise function:
+  #  y = math.floor(mx); where m is order.makerAmount/order.takerAmount
+  # 
+  # Because x is a natural number, the increments on the y-axis follow a peacewise function, where:
+  # yDelta = {
+  #            math.floor(order.makerAmount/order.takerAmount)  ; when (mx)%y != 0
+  #            math.ceil(order.makerAmount/order.takerAmount)   ; when mx%y == 0
+  #          } the y-values increment units at a time 
+  # 
+  # In the case of order matching where the left order is fully filled, we instead have:
+  #  y = math.ceil(mx)
+  #  yDelta = math.ceil(order.makerAmount/order.takerAmount)
+  # 
+  # It follows that yDelta differs between order matching and exchange.fillOrder
+  # iff (mx)%y != 0
+  # And,
+  # If yDelta differs for a given x, then so does y.
+  #
+  # This function seeks to find a value y that exists in oredr matching that 
+  # cannot exist in exchange.fillOrder. 
+  # That is:
+  #   There exists x_1 such that y_1 = matc.ceil(mx_1), and 
+  #   there exists no x_2 such that y_1 = math.floor(mx_2)
+  # 
+  # The input `c` to this function has a fixed x_1 and y_1.
+  # We return True iff no such x_2 exists.
+  #
+  
+  # Search for such an x_2 starting at x_1
+  x_1 = math.ceil((c.rightTaker * c.leftTaker) /c.rightMaker)
+  y_1 = c.leftTaker
+  x_2 = x_1
+  y = math.floor((c.rightMaker * x_2) / c.rightTaker)
+  if y == y_1:
+    return True
+  elif y < y_1:
+    while y < y_1:
+      x_2 += 1
+      y = math.floor((c.rightMaker * x_2) / c.rightTaker)
+    if y == y_1:
+      return True
+    return False
+  else: # y > y_1
+    while y > y_1:
+      x_2 -= 1
+      y = math.floor((c.rightMaker * x_2) / c.rightTaker)
+    if y == y_1:
+      return True
+    return False
+
+  raise Exception('Search failed. Should not hit this point')
+  
+def matchingResultNotAttainableByFillOrder_RightFillsLeft(c):
+  return not(matchingResultIsAttainableByFillOrder_RightFillsLeft(c))
+
+# Returns True iff there exists a `rightTaker` that could be returned by `Exchange.fillOrder`,
+# given `leftMaker`, `leftTaker`, and `rightMaker`
+def isValidFillResult_Left(c):
+  for i in range(1, c.leftMaker + 1):
+    if math.floor((c.leftTaker / c.leftMaker) * i) == c.rightTaker:
       return True
   return False
 
-def notValidFillResult_Right(c):
-  return not(isValidFillResult_Right(c))
+def notValidFillResult_Left(c):
+  return not(isValidFillResult_Left(c))
 
 def search(
   filters,
@@ -183,6 +258,30 @@ def test5():
     hasNegativeSpread,
     isRightFillsLeft,
     noRoundingErrorCeil_RightFillsLeft,
-    notValidFillResult_Right
+    matchingResultNotAttainableByFillOrder_RightFillsLeft
   ])
 test5()
+
+'''
+def test6():
+  print("Should transfer correct amounts when left order fill amount deviates from amount derived by `Exchange.fillOrder`'")
+  search([
+    hasNegativeSpread,
+    isLeftFillsRight,
+    noRoundingErrorFloor_LeftFillsRight,
+    notValidFillResult_Left
+  ])
+test6()
+'''
+
+'''
+for x in range(100000, 100100):
+  for y in range(x+1, 100100):
+    foundA = False
+    for a in range(2, x):
+      if (y * a) % x == 0 and (y % x != 0):
+        foundA = True
+        break
+    if not foundA:
+      print "(x=%d, y=%d, a=%d)"%(x, y, 1)
+        '''
